a854da0ee002269ff31b5da190ddc19cdd08f308
Bukavs <126393518+Bukavs@users.noreply.github.com>
2023-06-27 18:56:10 +0300

    Update book_edit.blade.php, Authors sorted by alphabet.

--- resources/views/book_edit.blade.php
+++ resources/views/book_edit.blade.php
@@ -43,7 +43,7 @@
                             <div class="p-6">
                                 <label for="author_id">{{__("Author")}}</label>
                                 <br>
-                                @foreach ($authors as $author)
+                                @foreach ($authors->sortBy('name') as $author)
                                     <label>
                                         <input type="checkbox" name="authors[]" value="{{ $author->id }}" {{ $book->authors->contains($author) ? 'checked' : '' }}>
                                         {{ $author->name }}
@@ -54,7 +54,7 @@
 
                         </fieldset>
                         <div class="px-6 py-4 text-right">
-                            <button class="border max-w-7xl mx-auto sm:px-6 lg:px-8 text-right" type="submit">{{__("Save")}}</button>
+                            <button class="border max-w-7xl mx-auto sm:px-6 lg:px-8 text-right" style="background-color:lavender" type="submit">{{__("Save")}}</button>
                         </div>
                     </form>
 
