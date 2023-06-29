0f0e748583988fd8a2873417055ddab0d8ad0b8f
inesemel <inese.melehova@inbox.lv>
2023-06-29 23:05:31 +0300

    Valodu atbalsts strādā

--- app/Http/Controllers/LanguageController.php
+++ app/Http/Controllers/LanguageController.php
@@ -4,19 +4,18 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\App;
+use Illuminate\Support\Facades\Session;
 
 class LanguageController extends Controller
 {
 
     public function changeLanguage(Request $request)
-{
-    $locale = $request->input('locale');
-    if (in_array($locale, config('app.locales'))) {
+    {
+        $locale = $request->input('locale');
         App::setLocale($locale);
+        Session::put("locale", $locale);
+        return redirect()->back();
     }
-
-    return redirect()->back();
-}
     /**
      * Display a listing of the resource.
      */
